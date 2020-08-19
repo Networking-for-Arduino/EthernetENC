94ee9e2ed176e97897a81a1212af71ea8e173c18
Juraj Andrassy <juraj.andrassy@gmail.com>
2020-08-19 07:29:30 +0200

    Dhcp.cpp fix - IP array compare to 0 crashed on some architectures

--- Dhcp.cpp
+++ Dhcp.cpp
@@ -327,7 +327,7 @@ uint8_t DhcpClass::parseDHCPResponse(unsigned long responseTimeout, uint32_t& tr
                 
                 case dhcpServerIdentifier :
                     opt_len = _dhcpUdpSocket.read();
-                    if( *((uint32_t*)_dhcpDhcpServerIp) == 0 || 
+                    if( _dhcpDhcpServerIp[0] == 0 ||
                         IPAddress(_dhcpDhcpServerIp) == _dhcpUdpSocket.remoteIP() )
                     {
                         _dhcpUdpSocket.read(_dhcpDhcpServerIp, sizeof(_dhcpDhcpServerIp));
